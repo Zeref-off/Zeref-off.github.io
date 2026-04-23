c85ce16cc4eeed0d69f39801c277e33e199e04a2
Zeref-off <soriamijael487@gmail.com>
2026-04-23 00:40:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const videoInput = document.getElementById("videoInput");
-const videoPlayer = document.getElementById("videoPlayer");
 
-videoInput.addEventListener("change", function() {
-    const file = this.files[0];
-    
-    if (file) {
-        const videoURL = URL.createObjectURL(file);
-        videoPlayer.src = videoURL;
-        videoPlayer.style.display = "block";
-    }
-});
